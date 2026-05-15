b9bb1795a2634d5ed80d000553be55f7c23c1bf7
jclencioni <juancruzlencioni@gmail.com>
2026-05-15 20:23:48 -0300

    Add files via upload

--- carta.js
+++ carta.js
@@ -6,10 +6,16 @@ class Carta{
 
 
     constructor(code, value, suit, imagen){
+        
+        if(!code || !value || !suit || !imagen){
+            throw new Error("Falta algun atributo.");
+        }
+        
         this._code = code;
         this._value = value
         this._suit = suit;
         this._imagen = imagen;
+    
     }
 
     set code(valor){
@@ -17,6 +23,10 @@ class Carta{
     }
 
     set value(valor){
+        if(isNaN(valor) || valor < 0){
+            throw new Error("El valor de la carta es incorrecto.");
+        }
+    
         this._value = valor;
     } 
 
@@ -27,5 +37,33 @@ class Carta{
     set imagen(valor){
         this._imagen = valor;
     }
+
+    toJsonString(){
+        return JSON.stringify(this);
+    }   
+
+    static createFromJsonString(json){
+        var nuevaCarta = JSON.parse(json);
+        return new Carta(nuevaCarta.code, nuevaCarta.value, nuevaCarta.suit, nuevaCarta.image)
+    }
+
+    createHTMLElement(){
+        var div = document.createElement("div");
+        div.setAttribute("class", "imagen-carta")
+        var codeCarta = document.createElement("h3");
+        codeCarta.appendChild(document.createTextNode(this._code));
+        var imagenCarta = document.createElement("img");
+        imagenCarta.setAttribute("src", this._imagen);
+        var suitCarta = document.createElement("h4");
+        suitCarta.appendChild(document.createTextNode(this._suit));
+        var valueCarta = document.createElement("h4");
+        valueCarta.appendChild(document.createTextNode(this._value));
+        div.appendChild(codeCarta);
+        div.appendChild(imagenCarta);
+        div.appendChild(suitCarta);
+        div.appendChild(valueCarta);
+        return div;
+    }
+
 
 }
