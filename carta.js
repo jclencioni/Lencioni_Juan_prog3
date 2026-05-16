491fb192d746e918ec21306ec6eeacaa4ad236b4
jclencioni <juancruzlencioni@gmail.com>
2026-05-15 21:40:32 -0300

    Add files via upload

--- carta.js
+++ carta.js
@@ -1,8 +1,8 @@
 class Carta{
-    _code
-    _value
-    _suit
-    _imagen
+    code
+    value
+    suit
+    image
 
 
     constructor(code, value, suit, imagen){
@@ -11,15 +11,15 @@ class Carta{
             throw new Error("Falta algun atributo.");
         }
         
-        this._code = code;
-        this._value = value
-        this._suit = suit;
-        this._imagen = imagen;
+        this.code = code;
+        this.value = value
+        this.suit = suit;
+        this.image = imagen;
     
     }
 
     set code(valor){
-        this._code=valor;
+        this.code=valor;
     }
 
     set value(valor){
@@ -27,20 +27,38 @@ class Carta{
             throw new Error("El valor de la carta es incorrecto.");
         }
     
-        this._value = valor;
+        this.value = valor;
     } 
 
     set suit(valor){
-        this._suit = valor;
+        this.suit = valor;
     }
 
     set imagen(valor){
-        this._imagen = valor;
+        this.image = valor;
     }
 
     toJsonString(){
         return JSON.stringify(this);
     }   
+
+    static guardarCarta(carta){
+        let storage = JSON.parse(localStorage.getItem("misCartas"));
+        if(storage.length > 0){
+            var coincidencia = storage.filter( e => {
+                return e.code == carta.code;
+            })
+            if(coincidencia.length > 0){
+                alert("La carta ya se encuentra guardada previamente.")
+            }else{
+                storage.push(carta);
+                localStorage.setItem("misCartas", JSON.stringify(storage));
+            }
+        }else{
+            storage.push(carta);
+            localStorage.setItem("misCartas", JSON.stringify(storage));
+        }
+    }
 
     static createFromJsonString(json){
         var nuevaCarta = JSON.parse(json);
@@ -51,17 +69,22 @@ class Carta{
         var div = document.createElement("div");
         div.setAttribute("class", "imagen-carta")
         var codeCarta = document.createElement("h3");
-        codeCarta.appendChild(document.createTextNode(this._code));
+        codeCarta.appendChild(document.createTextNode(this.code));
         var imagenCarta = document.createElement("img");
-        imagenCarta.setAttribute("src", this._imagen);
+        imagenCarta.setAttribute("src", this.image);
         var suitCarta = document.createElement("h4");
-        suitCarta.appendChild(document.createTextNode(this._suit));
+        suitCarta.appendChild(document.createTextNode(this.suit));
         var valueCarta = document.createElement("h4");
-        valueCarta.appendChild(document.createTextNode(this._value));
+        valueCarta.appendChild(document.createTextNode(this.value));
+        imagenCarta.addEventListener("click", () =>{ window.open(this.image, '_blank')})
+        var botonCarta = document.createElement("button");
+        botonCarta.appendChild(document.createTextNode("Guardar"));
+        botonCarta.addEventListener("click", () => Carta.guardarCarta(this));
         div.appendChild(codeCarta);
         div.appendChild(imagenCarta);
         div.appendChild(suitCarta);
         div.appendChild(valueCarta);
+        div.appendChild(botonCarta);
         return div;
     }
 
